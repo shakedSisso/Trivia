a9a334792debe84761d48f394165c2f4f327ff80
Ofer Ovadia <ofer026@gmail.com>
2023-04-27 23:22:25 +0300

    Added commenting to script

--- Trivia_Ofer_And_Shaked_Sisso_2023/client.py
+++ Trivia_Ofer_And_Shaked_Sisso_2023/client.py
@@ -6,13 +6,13 @@ LEN = 6
 def client():
     socket_to_server = socket.socket()
     try:
-        socket_to_server.connect(('localhost', PORT))
+        socket_to_server.connect(('localhost', PORT)) # using localhost refers to 127.0.0.1 which is a local address within the same machine
     except ConnectionRefusedError:
         print("Couldn't connect to server!")
         return
     print("Connected to server. sending hello")
-    socket_to_server.send("hello".encode())
-    msg = socket_to_server.recv(LEN).decode()
+    socket_to_server.send("hello".encode()) # using encode since the sockets need to receive data in binary
+    msg = socket_to_server.recv(LEN).decode() # using decode since the sockets returns data received in binary
     print("Recieved from server:", msg)
     socket_to_server.close()
 
