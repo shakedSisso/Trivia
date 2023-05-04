7ae67dfba27f4f8f74c83d6150f8468594fdabfa
Shaked Sisso <sissoshaked@gmail.com>
2023-05-04 10:08:20 +0300

    Added signatures for the functions in LoginRequestHandler. Created and fully implemented the functions in the source file LoginRequestHandler.

--- Trivia_Ofer_And_Shaked_Sisso_2023/LoginRequestHandler.h
+++ Trivia_Ofer_And_Shaked_Sisso_2023/LoginRequestHandler.h
@@ -1,7 +1,12 @@
 #pragma once
 #include <iostream>
 #include "IRequestHandler.h"
+#include "Requests.h"
 
-class LoginRequestHandler : IRequestHandler
+class LoginRequestHandler : public IRequestHandler
 {
+public:
+	LoginRequestHandler() = default;
+	bool isRequestRelevent(RequestInfo info) override;
+	RequestResult handleRequest(RequestInfo info) override;
 };
