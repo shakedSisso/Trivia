576a9733f381aab90ca712281fa207e3dd999845
Shaked Sisso <sissoshaked@gmail.com>
2023-05-08 20:51:39 +0300

    Added function comment in the Header file of JsonRequestPacketDeserializer class

--- Trivia_Ofer_And_Shaked_Sisso_2023/JsonRequestPacketDeserializer.h
+++ Trivia_Ofer_And_Shaked_Sisso_2023/JsonRequestPacketDeserializer.h
@@ -12,5 +12,17 @@ public:
 	static LoginRequest deserializeLoginRequest(const Buffer& buffer);
 	static SignupReqest deserializeSignupRequest(const Buffer& buffer);
 private:
+    /*
+	* Function: static extractIntFromBuffer
+	* ----------------------------
+	*	The function gets a buffer (vector of unsigned chars), an index and the amount of bytes. 
+	*	The function extracts from the buffer the number in the amount of bytes given from the index.
+	*
+	*	Buffer& buffer: the buffer to insert the number to
+	*	const int index: the index to start the extracting from
+	*	const int bytes: the amount of bytes the number will take
+	*
+	*	returns: void.
+   */
 	static int extractIntFromBuffer(const Buffer& buffer, const int index, const int bytes);
 };
