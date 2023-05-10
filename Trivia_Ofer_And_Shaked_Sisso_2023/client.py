15830589d35123c8c556e82d28d557a15025beeb
Ofer Ovadia <ofer026@gmail.com>
2023-05-10 18:15:39 +0300

    Adjusted python client script to v1.0.2

--- Trivia_Ofer_And_Shaked_Sisso_2023/client.py
+++ Trivia_Ofer_And_Shaked_Sisso_2023/client.py
@@ -1,7 +1,12 @@
 import socket
+import json
+import struct
 
 PORT = 1444
-LEN = 6
+CODE_LEN = 1
+JSON_LENGTH = 4
+LOGIN_REQUEST = 1
+SIGNUP_REQUEST = 2
 
 def client():
     socket_to_server = socket.socket()
@@ -11,10 +16,25 @@ def client():
         print("Couldn't connect to server!")
         return
     print("Connected to server on localhost:{}".format(PORT))
-    msg = socket_to_server.recv(LEN).decode() # using decode since the sockets returns data received in binary
-    print("Recieved from server:", msg)
-    print("Sending hello")
-    socket_to_server.send("hello".encode()) # using encode since the sockets need to receive data in binary
+    selection = input("Enter s for signup and l for login: ")
+    if selection == "s":
+        data = {'username': "user1", 'password': "1234", "mail": "test@gmail.com"}
+        request = str(chr(SIGNUP_REQUEST))
+    elif selection == 'l':
+        data = {'username': "user1", 'password': "1234"}
+        request = str(chr(LOGIN_REQUEST))
+    dataJson = json.dumps(data)
+
+    request += struct.pack('!I', len(dataJson))
+    request += dataJson
+    print("Sending request:")
+    print(request)
+    socket_to_server.send(request.encode())
+    response_code = socket_to_server.recv(CODE_LEN).decode() # using decode since the sockets returns data received in binary
+    response_json_length_bytes = socket_to_server.recv(JSON_LENGTH).decode()
+    response_json_length = struct.unpack('!I', response_json_length_bytes)
+    response_json = socket_to_server.recv(response_json_length).decode()
+    print("Received from server:", response_json)
     socket_to_server.close()
 
 def main():
