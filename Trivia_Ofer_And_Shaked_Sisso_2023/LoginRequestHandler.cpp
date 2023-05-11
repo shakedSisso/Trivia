c3d9af984596d2a4c38dbf5e58a2b3cca23092a8
Shaked Sisso <sissoshaked@gmail.com>
2023-05-11 13:32:00 +0300

    Moved error response into catch to catch errors from the deserializer and added throw of exception if request is irrelevent

--- Trivia_Ofer_And_Shaked_Sisso_2023/LoginRequestHandler.cpp
+++ Trivia_Ofer_And_Shaked_Sisso_2023/LoginRequestHandler.cpp
@@ -1,6 +1,8 @@
 #include "LoginRequestHandler.h"
 #include "MenuRequestHandler.h"
 #include "JsonResponsePacketSerializer.h"
+#include "JsonRequestPacketDeserializer.h"
+#include <exception>
 
 #define RESPONSE_STATUS_LOGIN 1
 #define RESPONSE_STATUS_SIGN_UP 2
@@ -18,31 +20,40 @@ RequestResult LoginRequestHandler::handleRequest(RequestInfo info)
 {
     RequestResult result;
     Buffer buffer;
-    if (isRequestRelevent(info))
-    {
-        MenuRequestHandler* menuHandler = new MenuRequestHandler();
-        result.newHandler = (IRequestHandler*)menuHandler;
-            if (info.id == SIGN_UP_REQUEST)
+    try {
+        if (isRequestRelevent(info))
         {
+            MenuRequestHandler* menuHandler = new MenuRequestHandler();
+            result.newHandler = (IRequestHandler*)menuHandler;
+            if (info.id == SIGN_UP_REQUEST)
+            {
                 SignupRequest signUp = JsonRequestPacketDeserializer::deserializeSignupRequest(info.buffer);
-            SignupResponse response;
-            response.status = RESPONSE_STATUS_SIGN_UP;
-            buffer = JsonResponsePacketSerializer::serializeResponse(response);
+                SignupResponse response;
+                response.status = RESPONSE_STATUS_SIGN_UP;
+                buffer = JsonResponsePacketSerializer::serializeResponse(response);
+            }
+            else
+            {
+                LoginRequest login = JsonRequestPacketDeserializer::deserializeLoginRequest(info.buffer);
+                LoginResponse response;
+                response.status = RESPONSE_STATUS_LOGIN;
+                buffer = JsonResponsePacketSerializer::serializeResponse(response);
+            }
         }
         else
         {
-                LoginRequest login = JsonRequestPacketDeserializer::deserializeLoginRequest(info.buffer);
-            LoginResponse response;
-            response.status = RESPONSE_STATUS_LOGIN;
-            buffer = JsonResponsePacketSerializer::serializeResponse(response);
+            throw std::exception("Request is irrelevent");
         }
     }
-    else
+    catch (const std::exception& e)
     {
+        std::cerr << e.what() << std::endl;
         result.newHandler = this;
         ErrorResponse response;
         response.message = "ERROR";
         buffer = JsonResponsePacketSerializer::serializeResponse(response);
     }
+
+    result.buffer = buffer;
     return result;
 }
