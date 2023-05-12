28b4e444547fdb654f1fd5894d90362236f9019e
Shaked Sisso <sissoshaked@gmail.com>
2023-05-13 01:18:07 +0300

    Fully implemented the constractor for the server class

--- Trivia_Ofer_And_Shaked_Sisso_2023/Server.cpp
+++ Trivia_Ofer_And_Shaked_Sisso_2023/Server.cpp
@@ -2,6 +2,14 @@
 #include <thread>
 #include <iostream>
 
+Server::Server() : m_communicator(this->m_handlerFactory), m_handlerFactory(nullptr)
+//putting null in the the database variable because the database doesn't exist yet
+{
+	this->m_database = new SqliteDatabase();
+	this->m_database->open();
+	this->m_handlerFactory = RequestHandlerFactory(this->m_database);
+}
+
 void Server::run()
 {
 	std::thread connectorThread(&Communicator::startHandleRequests, &this->m_communicator);
