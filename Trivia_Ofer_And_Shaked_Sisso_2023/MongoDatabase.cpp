a76130d2be2fbf95ea283a6457fe03768270c1e2
Ofer Ovadia <ofer026@gmail.com>
2023-05-16 22:49:58 +0300

    Implemented the mongo db class with the current version db functions

--- Trivia_Ofer_And_Shaked_Sisso_2023/MongoDatabase.cpp
+++ Trivia_Ofer_And_Shaked_Sisso_2023/MongoDatabase.cpp
@@ -1,12 +1,32 @@
 #include "MongoDatabase.h"
 
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <bsoncxx/oid.hpp>
+#include <bsoncxx/stdx/string_view.hpp>
+
+constexpr char MONGO_DB_URI[] = "mongodb://localhost:27017";
+constexpr char DATABASE_NAME[] = "triviaDB";
+#define USERS_COLLECTION_NAME "users"
+
+using mongocxx::collection;
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::stream::finalize;
+
+typedef bsoncxx::builder::basic::document basicDocument;
+typedef bsoncxx::builder::stream::document streamDocument;
+
 MongoDatabase::MongoDatabase()
 {
 }
 
 bool MongoDatabase::open()
 {
-	return false;
+	this->_uri = mongocxx::uri();
+	this->_client = mongocxx::client(this->_uri);
+	bsoncxx::string::view_or_value database_name{ "triviaDB" };
+	this->_db = this->_client[database_name];
+	return true;
 }
 
 bool MongoDatabase::close()
@@ -16,15 +36,57 @@ bool MongoDatabase::close()
 
 int MongoDatabase::doesUserExist(const std::string username)
 {
-	return 0;
+	collection usersCollections = this->_db[USERS_COLLECTION_NAME];
+	auto builder = streamDocument{};
+	bsoncxx::document::value queryDocument =
+		builder << "username" << username
+		<< finalize;
+
+	mongocxx::cursor cursor = usersCollections.find(queryDocument.view());
+
+	if (cursor.begin() != cursor.end()) 
+	{
+		return FOUND_RESPONSE_CODE;
+		std::cout << "Document exists!\n";
+	}
+	return ERROR_RESPONSE_CODE;
+	std::cout << "Document does not exist.\n";
 }
 
 int MongoDatabase::doesPasswordMatch(const std::string username, const std::string password)
 {
+	collection usersCollections = this->_db[USERS_COLLECTION_NAME];
+	auto builder = streamDocument{};
+	bsoncxx::document::value queryDocument =
+		builder << "username" << username
+		<< "password" << password
+		<< finalize;
+
+	mongocxx::cursor cursor = usersCollections.find(queryDocument.view());
+
+	if (cursor.begin() != cursor.end())
+	{
+		return FOUND_RESPONSE_CODE;
+		std::cout << "Document exists!\n";
+	}
+	return ERROR_RESPONSE_CODE;
+	std::cout << "Document does not exist.\n";
 	return 0;
 }
 
-int MongoDatabase::addNewUser(const std::string username, const std::string password, const std::string mail)
+int MongoDatabase::addNewUser(const std::string username, const std::string password, const std::string mail, const std::string address, const std::string phoneNumber, const std::string birthDate)
 {
+	collection usersCollections = this->_db[USERS_COLLECTION_NAME];
+
+	basicDocument documentBuilder{};
+	documentBuilder.append(kvp("username", username));
+	documentBuilder.append(kvp("password", password));
+	documentBuilder.append(kvp("email", mail));
+	documentBuilder.append(kvp("address", address));
+	documentBuilder.append(kvp("phone_number", phoneNumber));
+	documentBuilder.append(kvp("birth_date", birthDate));
+	bsoncxx::document::value doc = documentBuilder.extract();
+
+	usersCollections.insert_one(doc.view());
 	return 0;
 }
--- Trivia_Ofer_And_Shaked_Sisso_2023/MongoDatabase.h
+++ Trivia_Ofer_And_Shaked_Sisso_2023/MongoDatabase.h
@@ -1,6 +1,11 @@
 #pragma once
 #include "IDatabase.h"
 
+#include <mongocxx/client.hpp>
+#include <mongocxx/database.hpp>
+#include <mongocxx/uri.hpp>
+
+
 #define ERROR_RESPONSE_CODE 1
 #define FOUND_RESPONSE_CODE 2
 
@@ -12,6 +17,11 @@ public:
 	bool close() override;
 	int doesUserExist(const std::string username) override;
 	int doesPasswordMatch(const std::string username, const std::string password) override;
-	int addNewUser(const std::string username, const std::string password, const std::string mail) override;
+	int addNewUser(const std::string username, const std::string password, const std::string mail, const std::string address, const std::string phoneNumber, const std::string birthDate) override;
+
+private:
+	mongocxx::uri _uri;
+	mongocxx::client _client;
+	mongocxx::database _db;
 
 };
