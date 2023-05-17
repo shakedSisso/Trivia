f048d240a66c7da3ae0fad24b5dea75a0c80ca5a
Ofer Ovadia <ofer026@gmail.com>
2023-05-17 20:16:56 +0300

    Made the server class singleton

--- Trivia_Ofer_And_Shaked_Sisso_2023/Server.cpp
+++ Trivia_Ofer_And_Shaked_Sisso_2023/Server.cpp
@@ -2,12 +2,19 @@
 #include <thread>
 #include <iostream>
 
+int Server::instanceCount = 0;
+
 Server::Server() : m_communicator(this->m_handlerFactory), m_handlerFactory(nullptr)
 //putting null in the the database variable because the database doesn't exist yet
 {
+	if (instanceCount != 0)
+	{
+		throw std::exception("Server was already created once.");
+	}
 	this->m_database = new SqliteDatabase();
 	this->m_database->open();
 	this->m_handlerFactory = RequestHandlerFactory(this->m_database);
+	instanceCount++;
 }
 
 void Server::run()
