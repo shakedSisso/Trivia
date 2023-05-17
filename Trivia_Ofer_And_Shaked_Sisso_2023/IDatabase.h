17ec64ce4c77dd30eb08dd66aa13f8e338c8f326
Ofer Ovadia <ofer026@gmail.com>
2023-05-17 20:15:19 +0300

    Made IDatabase class singleton

--- Trivia_Ofer_And_Shaked_Sisso_2023/IDatabase.h
+++ Trivia_Ofer_And_Shaked_Sisso_2023/IDatabase.h
@@ -4,9 +4,12 @@
 class IDatabase
 {
 public:
+	IDatabase();
 	virtual bool open() = 0;
 	virtual bool close() = 0;
 	virtual int doesUserExist(const std::string username) = 0;
 	virtual int doesPasswordMatch(const std::string username, const std::string password) = 0;
 	virtual int addNewUser(const std::string username, const std::string password, const std::string mail) = 0;
+private:
+	static int instanceCount;
 };
