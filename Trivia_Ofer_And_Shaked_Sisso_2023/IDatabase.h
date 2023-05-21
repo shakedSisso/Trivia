7a84667d12bd2b2347e3f3c5c5011a6dc8a12dc4
Shaked Sisso <sissoshaked@gmail.com>
2023-05-21 20:18:51 +0300

    Added statistics to the SqliteDatabase class

--- Trivia_Ofer_And_Shaked_Sisso_2023/IDatabase.h
+++ Trivia_Ofer_And_Shaked_Sisso_2023/IDatabase.h
@@ -1,5 +1,10 @@
 #pragma once
 #include <iostream>
+#include "Globals.h"
+#include <list>
+#include <vector>
+
+#define HIGH_SCORES_COUNT 3
 
 class IDatabase
 {
@@ -10,7 +15,12 @@ public:
 	virtual int doesUserExist(const std::string username) = 0;
 	virtual int doesPasswordMatch(const std::string username, const std::string password) = 0;
 	virtual int addNewUser(const std::string username, const std::string password, const std::string mail, const std::string address, const std::string phoneNumber, const std::string birthDate) = 0;
-
+	virtual std::list<Question> getQuestions(int amountOfQuestions) = 0;
+	virtual float getPlayerAverageAnswerTime(std::string username) = 0;
+	virtual int getNumOfCorrectAnswers(std::string username) = 0;
+	virtual int getNumOfPlayerGames(std::string username) = 0;
+	virtual int getPlayerScore(std::string username) = 0;
+	virtual std::vector<std::string> getHighScores() = 0;
 private:
 	static int instanceCount;
 };
--- Trivia_Ofer_And_Shaked_Sisso_2023/SqliteDatabase.h
+++ Trivia_Ofer_And_Shaked_Sisso_2023/SqliteDatabase.h
@@ -16,6 +16,12 @@ public:
 	int doesUserExist(const std::string username) override;
 	int doesPasswordMatch(const std::string username, const std::string password) override;
 	int addNewUser(const std::string username, const std::string password, const std::string mail, const std::string address, const std::string phoneNumber, const std::string birthDate) override;
+	std::list<Question> getQuestions(int amountOfQuestions) override;
+	float getPlayerAverageAnswerTime(std::string username) override;
+	int getNumOfCorrectAnswers(std::string username) override;
+	int getNumOfPlayerGames(std::string username) override;
+	int getPlayerScore(std::string username) override;
+	std::vector<std::string> getHighScores() override;
 private:
 	sqlite3* _db;
 	std::string _dbFileName;
@@ -25,4 +31,5 @@ private:
 
 	bool createTables(int& res);
 	static int callbackString(void* list, int argc, char** argv, char** azColName);
+	static int callbackQuestions(void* list, int argc, char** argv, char** azColName);
 };
