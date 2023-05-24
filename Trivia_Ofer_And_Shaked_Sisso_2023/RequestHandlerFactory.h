832918d5cd0388da1b1c133592ea7ff84086e86b
Shaked Sisso <sissoshaked@gmail.com>
2023-05-24 17:28:24 +0300

    Added and fully implemented the functions related to V2.0.0 in RequestHandlerFactory class

--- Trivia_Ofer_And_Shaked_Sisso_2023/RequestHandlerFactory.h
+++ Trivia_Ofer_And_Shaked_Sisso_2023/RequestHandlerFactory.h
@@ -1,20 +1,31 @@
 #pragma once
 #include "SqliteDatabase.h"
 #include "LoginManager.h"
+#include "RoomManager.h"
+#include "StatisticsManager.h"
 #include "LoginRequestHandler.h"
+#include "MenuRequestHandler.h"
 
 class LoginRequestHandler;
+class MenuRequestHandler;
 
 class RequestHandlerFactory
 {
 public:
 	RequestHandlerFactory(IDatabase* database);
 	LoginRequestHandler* createLoginRequestHandler();
-	LoginManager* getLoginManager();
+	MenuRequestHandler* createMenuRequestHandler(LoggedUser& user);
+
+	LoginManager& getLoginManager();
+	RoomManager& getRoomManager();
+	StatisticsManager& getStatisticsManager();
+
 	void setDatabase(IDatabase* db);
 
 private:
 	static int instanceCount;
 	LoginManager m_loginManager;
+	RoomManager m_roomManager;
+	StatisticsManager m_statisticsManager;
 	IDatabase* m_database;
 };
