291513a1af85e13ef02021748b6abab91ae509a3
Shaked Sisso <sissoshaked@gmail.com>
2023-05-28 16:14:40 +0300

    Moved the codes enum to the Globals file and added Error to the list of codes

--- Trivia_Ofer_And_Shaked_Sisso_2023/Globals.h
+++ Trivia_Ofer_And_Shaked_Sisso_2023/Globals.h
@@ -7,3 +7,6 @@ typedef int RequestId;
 typedef unsigned char Byte;
 typedef vector<Byte> Buffer;
 typedef unsigned int RoomID;
+
+
+enum codes { Error, Login, Signup, GetPlayersInRoom, JoinRoom, CreateRoom, HighScore, Logout, GetRooms, Statistics, CloseRoom, StartRoom, GetRoomState, LeaveRoom };
--- Trivia_Ofer_And_Shaked_Sisso_2023/Requests.h
+++ Trivia_Ofer_And_Shaked_Sisso_2023/Requests.h
@@ -5,8 +5,6 @@
 
 class IRequestHandler;
 
-enum requestCodes{ Login = 1, Signup, GetPlayersInRoom, JoinRoom, CreateRoom, HighScore, Logout, GetRooms, Statistics, CloseRoom, StartRoom, GetRoomState, LeaveRoom };
-
 typedef struct LoginRequest
 {
 	std::string username;
