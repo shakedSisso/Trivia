99ee4bc4a2c766235482d91720bf0f52ca1687a4
Shaked Sisso <sissoshaked@gmail.com>
2023-05-30 23:44:55 +0300

    Created and fully implemented the RoomAdminRequestHandler class

---
+++ Trivia_Ofer_And_Shaked_Sisso_2023/RoomAdminRequestHandler.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include "IRequestHandler.h"
+#include "RequestHandlerFactory.h"
+
+class RequestHandlerFactory;
+
+class RoomMemberRequestHandler : public IRequestHandler
+{
+public:
+	RoomMemberRequestHandler(RequestHandlerFactory& handlerFactory, Room& room, LoggedUser& user, RoomManager& roomManager);
+	bool isRequestRelevent(const RequestInfo& info) override;
+	RequestResult handleRequest(const RequestInfo& info) override;
+private:
+	Room m_room;
+	LoggedUser m_user;
+	RoomManager& m_roomManager;
+	RequestHandlerFactory& m_handlerFactory;
+
+	RequestResult closeRoom(const RequestInfo& info);
+	RequestResult startGame(const RequestInfo& info);
+	RequestResult getRoomState(const RequestInfo& info);
+};
