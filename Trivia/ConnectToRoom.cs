c0995836e625129e31f258220da02a11ef3a23fe
Ofer Ovadia <ofer026@gmail.com>
2023-05-31 23:32:07 +0300

    Added frequent updates of the rooms list

--- Trivia/ConnectToRoom.cs
+++ Trivia/ConnectToRoom.cs
@@ -7,20 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Trivia
 {
     public partial class ConnectToRoom : Form
     {
+
+        private Thread updateThread;
+        private bool threadFlag;
+        private string[] rooms;
         public ConnectToRoom()
         {
             InitializeComponent();
             this.StartPosition = FormStartPosition.CenterScreen;
+            try
+            {
+                this.rooms = Program.GetCommunicator().GetRooms();
+                updateRoomsList();
+                this.updateThread = new Thread(getUpdatedRoomList);
+                this.threadFlag = true;
+                this.updateThread.Start();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void updateRoomsList()
+        {
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            this.threadFlag = false;
+            this.updateThread?.Join();
             this.Dispose();
+        }
+
+        private void getUpdatedRoomList()
+        {
+            while(this.threadFlag)
+            {
+                this.rooms = Program.GetCommunicator().GetRooms();
+                updateRoomsList();
+                Thread.Sleep(3000);
+            }
         }
 
     }
