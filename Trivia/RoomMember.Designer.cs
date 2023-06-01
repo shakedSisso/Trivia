7a68c3507039c3a71bb40653658900664ec930d1
Shaked Sisso <sissoshaked@gmail.com>
2023-06-02 00:56:21 +0300

    Created the RoomMember form to the gui

--- Trivia/RoomMember.Designer.cs
+++ Trivia/RoomMember.Designer.cs
@@ -29,19 +29,92 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RoomMember));
+            lblRoomName = new Label();
+            lblTimeOut = new Label();
+            lblQuestionCount = new Label();
+            gbPlayers = new GroupBox();
+            btnLeaveGame = new Button();
             SuspendLayout();
+            // 
+            // lblRoomName
+            // 
+            lblRoomName.AutoSize = true;
+            lblRoomName.Font = new Font("Bauhaus 93", 25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblRoomName.ForeColor = Color.DarkSlateGray;
+            lblRoomName.Location = new Point(173, 20);
+            lblRoomName.Name = "lblRoomName";
+            lblRoomName.Size = new Size(419, 48);
+            lblRoomName.TabIndex = 52;
+            lblRoomName.Text = "You are connected to";
+            // 
+            // lblTimeOut
+            // 
+            lblTimeOut.AutoSize = true;
+            lblTimeOut.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            lblTimeOut.Location = new Point(24, 178);
+            lblTimeOut.Name = "lblTimeOut";
+            lblTimeOut.Size = new Size(178, 25);
+            lblTimeOut.TabIndex = 51;
+            lblTimeOut.Text = "Time per question:";
+            // 
+            // lblQuestionCount
+            // 
+            lblQuestionCount.AutoSize = true;
+            lblQuestionCount.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            lblQuestionCount.Location = new Point(24, 83);
+            lblQuestionCount.Name = "lblQuestionCount";
+            lblQuestionCount.Size = new Size(206, 25);
+            lblQuestionCount.TabIndex = 50;
+            lblQuestionCount.Text = "Number of questions:";
+            // 
+            // gbPlayers
+            // 
+            gbPlayers.BackColor = Color.LightCyan;
+            gbPlayers.Font = new Font("Bauhaus 93", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            gbPlayers.Location = new Point(301, 83);
+            gbPlayers.Name = "gbPlayers";
+            gbPlayers.Size = new Size(433, 200);
+            gbPlayers.TabIndex = 48;
+            gbPlayers.TabStop = false;
+            gbPlayers.Text = "Current participants are:";
+            // 
+            // btnLeaveGame
+            // 
+            btnLeaveGame.BackColor = Color.CadetBlue;
+            btnLeaveGame.Font = new Font("Maiandra GD", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            btnLeaveGame.ForeColor = SystemColors.ButtonHighlight;
+            btnLeaveGame.Location = new Point(221, 329);
+            btnLeaveGame.Name = "btnLeaveGame";
+            btnLeaveGame.Size = new Size(292, 57);
+            btnLeaveGame.TabIndex = 46;
+            btnLeaveGame.Text = "Leave game";
+            btnLeaveGame.UseVisualStyleBackColor = false;
+            btnLeaveGame.Click += btnLeaveGame_Click;
             // 
             // RoomMember
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PaleTurquoise;
             ClientSize = new Size(800, 450);
+            Controls.Add(lblRoomName);
+            Controls.Add(lblTimeOut);
+            Controls.Add(lblQuestionCount);
+            Controls.Add(gbPlayers);
+            Controls.Add(btnLeaveGame);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "RoomMember";
             Text = "RoomMember";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblRoomName;
+        private Label lblTimeOut;
+        private Label lblQuestionCount;
+        private GroupBox gbPlayers;
+        private Button btnLeaveGame;
     }
 }
--- Trivia/RoomMember.cs
+++ Trivia/RoomMember.cs
@@ -12,10 +12,48 @@ namespace Trivia
 {
     public partial class RoomMember : Form
     {
-        public RoomMember()
+        private Thread updateThread;
+        private bool threadFlag;
+        private string roomName;
+        private int questionCount;
+        private int timePerQuestion;
+        public RoomMember(string name)
         {
             InitializeComponent();
             this.StartPosition = FormStartPosition.CenterScreen;
+            this.roomName = name;
+            lblRoomName.Text = "You are connected to " + this.roomName;
+            lblRoomName.Left = (this.Width - lblRoomName.Width - 20) / 2;
+            try
+            {
+                updatePlayersList();
+                this.updateThread = new Thread(updatePlayersList);
+                this.threadFlag = true;
+                this.updateThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void joinThread()
+        {
+            this.threadFlag = false;
+            this.updateThread?.Join();
+        }
+
+        private void updatePlayersList()
+        {
+            Program.GetCommunicator().GetRoomState();
+            //update list of players connected to the room
+        }
+
+        private void btnLeaveGame_Click(object sender, EventArgs e)
+        {
+            Program.GetCommunicator().LeaveRoom();
+            joinThread();
+            this.Dispose();
         }
     }
 }
