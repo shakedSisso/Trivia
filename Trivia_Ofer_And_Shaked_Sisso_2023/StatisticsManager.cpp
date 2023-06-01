a27a8c824a61dca548b43baa2e8318247793b854
Shaked Sisso <sissoshaked@gmail.com>
2023-06-02 00:52:54 +0300

    Fixed function getUserStatistics so average time string would only include 3 digits after the decimal point

--- Trivia_Ofer_And_Shaked_Sisso_2023/StatisticsManager.cpp
+++ Trivia_Ofer_And_Shaked_Sisso_2023/StatisticsManager.cpp
@@ -1,4 +1,9 @@
 #include "StatisticsManager.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#define DIGITS_AFTER_POINT 3
 
 StatisticsManager::StatisticsManager(IDatabase* database)
     : m_database(database)
@@ -17,7 +22,11 @@ vector<string> StatisticsManager::getUserStatistics(const string username) const
     int correctAnswers = 0;
     correctAnswers = this->m_database->getNumOfCorrectAnswers(username);
 
-    stats.push_back(std::to_string(this->m_database->getPlayerAverageAnswerTime(username)));
+    std::stringstream stream;
+    stream.precision(DIGITS_AFTER_POINT); // Set precision to the amount of digits after the decimal point the define has
+    stream << std::fixed << this->m_database->getPlayerAverageAnswerTime(username); // Convert the number to string with fixed-point notation
+
+    stats.push_back(stream.str());
     stats.push_back(std::to_string(correctAnswers));
     stats.push_back(std::to_string(this->m_database->getNumOfTotalAnswers(username) - correctAnswers));
     stats.push_back(std::to_string(this->m_database->getNumOfPlayerGames(username)));
