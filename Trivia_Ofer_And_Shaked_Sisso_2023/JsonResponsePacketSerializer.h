d45a0686a4a20eff2aa8890f91bb5f361547a5b4
Ofer Ovadia <ofer026@gmail.com>
2023-06-07 18:37:01 +0300

    Added new serialize function signature for new respones types in V4.0.0

--- Trivia_Ofer_And_Shaked_Sisso_2023/JsonResponsePacketSerializer.h
+++ Trivia_Ofer_And_Shaked_Sisso_2023/JsonResponsePacketSerializer.h
@@ -25,6 +25,11 @@ public:
     static Buffer serializeResponse(const StartGameResponse& response);
     static Buffer serializeResponse(const GetRoomStateResponse& response);
     static Buffer serializeResponse(const LeaveRoomResponse& response);
+
+    static Buffer serializeResponse(const GetGameResultsResponse& response);
+    static Buffer serializeResponse(const SubmitAnswerResponse& response);
+    static Buffer serializeResponse(const GetQuestionResponse& response);
+    static Buffer serializeResponse(const LeaveGameResponse& response);
     
 private:
     /*
