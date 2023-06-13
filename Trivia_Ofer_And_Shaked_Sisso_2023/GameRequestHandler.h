4da6df96a049bc4f15053163ecb8ffafb659b230
Shaked Sisso <sissoshaked@gmail.com>
2023-06-13 17:07:38 +0300

    Fixed GameRequestHandler class to avoid problems in the client

--- Trivia_Ofer_And_Shaked_Sisso_2023/GameRequestHandler.h
+++ Trivia_Ofer_And_Shaked_Sisso_2023/GameRequestHandler.h
@@ -2,18 +2,19 @@
 #include <iostream>
 #include "IRequestHandler.h"
 #include "RequestHandlerFactory.h"
+#include "Game.h"
 
 class RequestHandlerFactory;
 
 class GameRequestHandler : public IRequestHandler
 {
 public:
-	GameRequestHandler(RequestHandlerFactory& handlerFactory, GameManager& gameManager, Game game, LoggedUser& user);
+	GameRequestHandler(RequestHandlerFactory& handlerFactory, GameManager& gameManager, GameID gameId, LoggedUser& user);
 	bool isRequestRelevent(const RequestInfo& info) override;
 	RequestResult handleRequest(const RequestInfo& info) override;
 private:
-	Game& m_game;
-	LoggedUser& m_user;
+	Game m_game;
+	LoggedUser m_user;
 	RequestHandlerFactory& m_handlerFactory;
 	GameManager& m_gameManager;
 
