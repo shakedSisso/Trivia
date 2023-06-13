d070b63cecef4379d83b32f05de0e357c7b93148
Shaked Sisso <sissoshaked@gmail.com>
2023-06-13 17:15:38 +0300

    Created the Game form

--- Trivia/Game.Designer.cs
+++ Trivia/Game.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
             lblQuestionCount = new Label();
             lblRoomName = new Label();
@@ -37,6 +38,11 @@
             btnAnswer2 = new Button();
             btnAnswer3 = new Button();
             btnAnswer4 = new Button();
+            lblTimeOut = new Label();
+            lblScore = new Label();
+            label1 = new Label();
+            btnExit = new Button();
+            tmrCountdown = new System.Windows.Forms.Timer(components);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -45,7 +51,7 @@
             lblQuestionCount.AutoSize = true;
             lblQuestionCount.Font = new Font("Bauhaus 93", 20F, FontStyle.Regular, GraphicsUnit.Point);
             lblQuestionCount.ForeColor = Color.DarkSlateGray;
-            lblQuestionCount.Location = new Point(270, 68);
+            lblQuestionCount.Location = new Point(269, 55);
             lblQuestionCount.Name = "lblQuestionCount";
             lblQuestionCount.Size = new Size(207, 39);
             lblQuestionCount.TabIndex = 45;
@@ -65,7 +71,7 @@
             // pictureBox1
             // 
             pictureBox1.BackColor = Color.LightCyan;
-            pictureBox1.Location = new Point(87, 127);
+            pictureBox1.Location = new Point(90, 107);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(600, 285);
             pictureBox1.TabIndex = 47;
@@ -75,10 +81,10 @@
             // 
             lblQuestion.AutoSize = true;
             lblQuestion.BackColor = Color.LightCyan;
-            lblQuestion.Font = new Font("Segoe UI", 16F, FontStyle.Italic, GraphicsUnit.Point);
-            lblQuestion.Location = new Point(106, 147);
+            lblQuestion.Font = new Font("Segoe UI", 13F, FontStyle.Italic, GraphicsUnit.Point);
+            lblQuestion.Location = new Point(106, 119);
             lblQuestion.Name = "lblQuestion";
-            lblQuestion.Size = new Size(128, 37);
+            lblQuestion.Size = new Size(103, 30);
             lblQuestion.TabIndex = 48;
             lblQuestion.Text = "question?";
             // 
@@ -86,45 +92,97 @@
             // 
             btnAnswer1.BackColor = Color.MintCream;
             btnAnswer1.Font = new Font("Maiandra GD", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
-            btnAnswer1.Location = new Point(106, 207);
+            btnAnswer1.Location = new Point(110, 190);
             btnAnswer1.Name = "btnAnswer1";
             btnAnswer1.Size = new Size(558, 35);
             btnAnswer1.TabIndex = 49;
             btnAnswer1.Text = "answer1";
             btnAnswer1.UseVisualStyleBackColor = false;
+            btnAnswer1.Click += SharedButtonClick;
             // 
             // btnAnswer2
             // 
             btnAnswer2.BackColor = Color.MintCream;
             btnAnswer2.Font = new Font("Maiandra GD", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
-            btnAnswer2.Location = new Point(106, 254);
+            btnAnswer2.Location = new Point(110, 237);
             btnAnswer2.Name = "btnAnswer2";
             btnAnswer2.Size = new Size(558, 35);
             btnAnswer2.TabIndex = 50;
             btnAnswer2.Text = "answer2";
             btnAnswer2.UseVisualStyleBackColor = false;
+            btnAnswer2.Click += SharedButtonClick;
             // 
             // btnAnswer3
             // 
             btnAnswer3.BackColor = Color.MintCream;
             btnAnswer3.Font = new Font("Maiandra GD", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
-            btnAnswer3.Location = new Point(106, 306);
+            btnAnswer3.Location = new Point(110, 289);
             btnAnswer3.Name = "btnAnswer3";
             btnAnswer3.Size = new Size(558, 35);
             btnAnswer3.TabIndex = 51;
             btnAnswer3.Text = "answer3";
             btnAnswer3.UseVisualStyleBackColor = false;
+            btnAnswer3.Click += SharedButtonClick;
             // 
             // btnAnswer4
             // 
             btnAnswer4.BackColor = Color.MintCream;
             btnAnswer4.Font = new Font("Maiandra GD", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
-            btnAnswer4.Location = new Point(106, 354);
+            btnAnswer4.Location = new Point(110, 337);
             btnAnswer4.Name = "btnAnswer4";
             btnAnswer4.Size = new Size(558, 35);
             btnAnswer4.TabIndex = 52;
             btnAnswer4.Text = "answer4";
             btnAnswer4.UseVisualStyleBackColor = false;
+            btnAnswer4.Click += SharedButtonClick;
+            // 
+            // lblTimeOut
+            // 
+            lblTimeOut.AutoSize = true;
+            lblTimeOut.Font = new Font("Segoe UI Semibold", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            lblTimeOut.Location = new Point(720, 25);
+            lblTimeOut.Name = "lblTimeOut";
+            lblTimeOut.Size = new Size(49, 38);
+            lblTimeOut.TabIndex = 53;
+            lblTimeOut.Text = "00";
+            // 
+            // lblScore
+            // 
+            lblScore.AutoSize = true;
+            lblScore.Font = new Font("Segoe UI Semibold", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            lblScore.Location = new Point(128, 25);
+            lblScore.Name = "lblScore";
+            lblScore.Size = new Size(61, 38);
+            lblScore.TabIndex = 54;
+            lblScore.Text = "0/0";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(28, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 38);
+            label1.TabIndex = 55;
+            label1.Text = "Score:";
+            // 
+            // btnExit
+            // 
+            btnExit.BackColor = Color.CadetBlue;
+            btnExit.Font = new Font("Maiandra GD", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnExit.ForeColor = SystemColors.ButtonHighlight;
+            btnExit.Location = new Point(640, 409);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(148, 29);
+            btnExit.TabIndex = 56;
+            btnExit.Text = "exit";
+            btnExit.UseVisualStyleBackColor = false;
+            btnExit.Click += btnExit_Click;
+            // 
+            // tmrCountdown
+            // 
+            tmrCountdown.Interval = 1000;
+            tmrCountdown.Tick += tmrCountdown_Tick;
             // 
             // Game
             // 
@@ -132,6 +190,10 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.PaleTurquoise;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnExit);
+            Controls.Add(label1);
+            Controls.Add(lblScore);
+            Controls.Add(lblTimeOut);
             Controls.Add(btnAnswer4);
             Controls.Add(btnAnswer3);
             Controls.Add(btnAnswer2);
@@ -143,6 +205,7 @@
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Game";
             Text = "Game";
+            FormClosing += Game_FormClosing;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -158,5 +221,10 @@
         private Button btnAnswer2;
         private Button btnAnswer3;
         private Button btnAnswer4;
+        private Label lblTimeOut;
+        private Label lblScore;
+        private Label label1;
+        private Button btnExit;
+        private System.Windows.Forms.Timer tmrCountdown;
     }
 }
--- Trivia/Game.cs
+++ Trivia/Game.cs
@@ -13,14 +13,227 @@ namespace Trivia
 {
     public partial class Game : Form
     {
+        private const int QUESTION_LENGTH = 55;
         private string roomName;
-        public Game(string name)
+        private int questionTimeOut;
+        private int remainingSeconds;
+        private int questionCount;
+        private int currentCount;
+        private int correctAnswers;
+        private object communicatorLock;
+        private bool isLocked;
+        private bool nextQuestion;
+        private System.Threading.Timer timer;
+        public Game(string name, int questionTimeOut, int questionCount)
         {
             InitializeComponent();
             this.StartPosition = FormStartPosition.Manual;
             this.Location = LocationManager.GetFormLocation();
             this.Text = name;
             this.roomName = name;
+            this.questionTimeOut = questionTimeOut;
+            this.questionCount = questionCount;
+            this.communicatorLock = new object();
+            this.isLocked = false;
+            this.nextQuestion = true;
+            try
+            {
+                InitButtons();
+                timer = new System.Threading.Timer(ResetScreen, null, 0, 1000);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void InitButtons()
+        {
+            btnAnswer1.Tag = 1;
+            btnAnswer2.Tag = 2;
+            btnAnswer3.Tag = 3;
+            btnAnswer4.Tag = 4;
+        }
+
+        private void ChangeScore()
+        {
+            lblScore.Text = correctAnswers.ToString() + " / " + currentCount.ToString();
+        }
+
+        private void ResetScreen(object state)
+        {
+            if (this.IsHandleCreated && nextQuestion)
+            {
+                currentCount++;
+                this.Invoke((MethodInvoker)delegate
+                {
+                    ChangeScore();
+                });
+                if (currentCount < questionCount)
+                {
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        NextQuestion();
+                    });
+                }
+                else
+                {
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        OpenGameScoresForm();
+                    });
+                }
+
+            }
+        }
+
+        private void OpenGameScoresForm()
+        {
+            try
+            {
+                nextQuestion = false;
+                Form fGameScores = new GameScores();
+                if (timer != null)
+                {
+                    this.timer.Dispose();
+                    this.timer = null;
+                }
+                fGameScores.ShowDialog();
+                this.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void NextQuestion()
+        {
+            this.remainingSeconds = questionTimeOut;
+            lblTimeOut.Text = remainingSeconds.ToString();
+            tmrCountdown.Start();
+            foreach (Control control in this.Controls)
+            {
+                if (control is Button button && button != btnExit)
+                {
+                    control.BackColor = Color.MintCream;
+                    control.Enabled = true;
+                }
+            }
+            nextQuestion = false;
+            GetQuestion();
+        }
+
+        private void GetQuestion()
+        {
+            dynamic question;
+            lock (communicatorLock)
+            {
+                isLocked = true;
+                question = Program.GetCommunicator().GetQuestion();
+                isLocked = false;
+            }
+            if (question != null && question.question != "")
+            {
+                string q = question.question;
+
+                int charsPerLine = QUESTION_LENGTH;
+                int currentIndex = charsPerLine;
+
+                while (currentIndex < q.Length)
+                {
+                    int spaceIndex = q.LastIndexOf(' ', currentIndex, charsPerLine);
+
+                    if (spaceIndex != -1)
+                    {
+                        q = q.Remove(spaceIndex, 1).Insert(spaceIndex, "\n");
+                        currentIndex = spaceIndex + charsPerLine + 1;
+                    }
+                    else
+                    {
+                        currentIndex += charsPerLine;
+                    }
+                }
+                lblQuestion.Text = q;
+                lblQuestionCount.Text = "Question " + currentCount.ToString() + "/" + questionCount.ToString();
+                dynamic answers = question.answers.First;
+                btnAnswer1.Text = answers["1"];
+                btnAnswer2.Text = answers["2"];
+                btnAnswer3.Text = answers["3"];
+                btnAnswer4.Text = answers["4"];
+            }
+        }
+
+        private void SharedButtonClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            int answerId = (int)button.Tag;
+            try
+            {
+                int correctAnswer = Program.GetCommunicator().SubmitAnswer(answerId);
+                if (correctAnswer == answerId)
+                {
+                    button.BackColor = Color.PaleGreen;
+                    correctAnswers++;
+                    foreach (Control control in this.Controls)
+                    {
+                        if (control is Button btn && btn != btnExit)
+                        {
+                            btn.Enabled = false;
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (Control control in this.Controls)
+                    {
+                        if (control is Button btn && btn != btnExit)
+                        {
+                            btn.BackColor = Color.IndianRed;
+                            btn.Enabled = false;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            nextQuestion = true;
+        }
+
+        private void tmrCountdown_Tick(object sender, EventArgs e)
+        {
+            remainingSeconds--;
+            lblTimeOut.Text = remainingSeconds.ToString();
+
+            if (remainingSeconds <= 0)
+            {
+                nextQuestion = true;
+                tmrCountdown.Stop();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void Game_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Program.GetCommunicator().LeaveGame();
+                if (this.timer != null)
+                {
+                    this.timer.Dispose();
+                    this.timer = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
