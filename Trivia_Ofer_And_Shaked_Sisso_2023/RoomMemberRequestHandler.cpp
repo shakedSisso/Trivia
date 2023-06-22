a24dedd39e80576fe957471b598ae007b87bba7e
Shaked Sisso <sissoshaked@gmail.com>
2023-06-22 14:24:46 +0300

    Added a check if the room is empty and delete the room if it is empty (for head to head rooms)

--- Trivia_Ofer_And_Shaked_Sisso_2023/RoomMemberRequestHandler.cpp
+++ Trivia_Ofer_And_Shaked_Sisso_2023/RoomMemberRequestHandler.cpp
@@ -70,6 +70,12 @@ RequestResult RoomMemberRequestHandler::leaveRoom(const RequestInfo& info)
         try
         {
             this->m_room->removeUser(this->m_user);
+
+            if (this->m_room->getAllUsers().empty()) //Used in the head to head room since all the players are members
+            {
+                this->m_roomManager.deleteRoom(this->m_roomId);
+            }
+
             result.newHandler = (IRequestHandler*)this->m_handlerFactory.createMenuRequestHandler(this->m_user);
         }
         catch (const std::exception& e)
