f7c82494b5f3525f0dbd2301465bfd7d5133ea6e
Ofer Ovadia <ofer026@gmail.com>
2023-06-27 22:57:37 +0300

    Added documentation to the LoginManager header file

--- Trivia_Ofer_And_Shaked_Sisso_2023/LoginManager.h
+++ Trivia_Ofer_And_Shaked_Sisso_2023/LoginManager.h
@@ -11,11 +11,31 @@ public:
 	void signup(const std::string username, const std::string password, const std::string mail, const std::string address, const std::string phoneNumber, const std::string birthDate);
 	void login(const std::string username, const std::string password);
 	void logout(const std::string username);
+
+	/*
+	* Function: setDatabase
+	* ----------------------------
+	*   The function gets a pointer to an IDatabase object and sets the m_database pointer to the given pointer
+	*
+	*   IDatabase* database: The new pointer to the database to set
+	*
+	*   returns: void.
+	*/
 	void setDatabase(IDatabase* database);
 private:
 	static int instanceCount;
 	std::vector<LoggedUser> m_loggedUsers;
 	IDatabase* m_database;
 
-	bool isUserLoggedIn(const std::string username);
+	/*
+	* Function: isUserLoggedIn const
+	* ----------------------------
+	*   The function gets a username string and checks if the given username is in the vector of LoggedUsers 
+	*	and returns true if the user if found in the vector and false otherwise.
+	*
+	*   const std::string& username: The user to look for in the vector of LoggedUsers
+	*
+	*   returns: bool. true if the username is found and false otherwise
+	*/
+	bool isUserLoggedIn(const std::string& username) const;
 };
