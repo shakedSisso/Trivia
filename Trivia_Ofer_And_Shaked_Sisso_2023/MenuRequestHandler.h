9053f45e573e8ff314923a37fe3aa5b285ef72a8
Ofer Ovadia <ofer026@gmail.com>
2023-06-27 23:38:14 +0300

    Added documentation to the MenuRequestHandler header file

--- Trivia_Ofer_And_Shaked_Sisso_2023/MenuRequestHandler.h
+++ Trivia_Ofer_And_Shaked_Sisso_2023/MenuRequestHandler.h
@@ -29,6 +29,24 @@ private:
 	RequestResult addQuestion(const RequestInfo& info);
 	RequestResult joinHeadToHead(const RequestInfo& info);
 
+	/*
+	* Function: findHeadToHeadRoom const
+	* ----------------------------
+	*   The function gets a vector of rooms and searches for a room of headToHead mode and returns it's id if the rooms exists and HEAD_TO_HEAD_DOES_NOT_EXIST if not.
+	*
+	*   std::vector<RoomData> rooms: the vector of rooms to search for the id of the head to head room in
+	*
+	*   returns: int. The id of the head to head room is it exists and HEAD_TO_HEAD_DOES_NOT_EXIST if not
+	*/
 	int findHeadToHeadRoom(std::vector<RoomData> rooms) const;
+	/*
+	* Function: removeHeadToHeadRoomsFromTheList
+	* ----------------------------
+	*   The function gets a vector of rooms and removes the rooms of head to head mode from the vector
+	*
+	*   std::vector<RoomData> rooms: the vector of rooms
+	*
+	*   returns: void
+	*/
 	void removeHeadToHeadRoomsFromTheList(std::vector<RoomData>& rooms);
 };
