1979fd9ab0b5ac9c59a3ca52b0a2b16c394c1219
Shaked Sisso <sissoshaked@gmail.com>
2023-06-27 23:56:55 +0300

    Fixed the encyption class in the client

--- Trivia/RSACryptoAlgorithm.cs
+++ Trivia/RSACryptoAlgorithm.cs
@@ -1,208 +1,143 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Reflection;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Trivia
 {
     class RSACryptoAlgorithm
     {
-        private static BigInteger m_modulus;
-        private static BigInteger m_publicKey;
-        private static BigInteger m_privateKey;
+        public static int m_modulus;
+        public static int m_publicKey;
+        private static int m_privateKey;
 
         private static Random random = new Random();
 
-        public static void createKeys(int keyLength)
+        private static int GetRandomPrime(int min, int max)
         {
-            int p = getRandomPrime(2, 100);
-            int q = getRandomPrime(2, 100);
-            while (p == q)
+            Random rd = new Random();
+            int prime = rd.Next(min, max + 1);
+            while (!IsPrime(prime))
             {
-                q = getRandomPrime(2, 100);
+                prime = rd.Next(min, max + 1);
             }
 
-            m_modulus = p * q;
-            int phi = (p - 1) * (q - 1);
-
-            // Choose a public key (usually a prime number)
-            m_publicKey = getRandomPrime(2, phi);
-
-            // Ensure public key and phi are coprime
-            while (GCD(m_publicKey, phi) != 1)
-            {
-                m_publicKey = getRandomPrime(2, phi);
-            }
-
-            // Calculate the private key using modular inverse
-            m_privateKey = ModInverse(m_publicKey, phi);
+            return prime;
         }
 
-        public static BigInteger getPublicKey()
+        public static void CreateKeys()
+        {
+            List<int> keys = new List<int>(2);
+            int p = GetRandomPrime(100, 1000);
+            int q = GetRandomPrime(100, 1000);
+            while (p == q)
+            {
+                q = GetRandomPrime(2, 100);
+            }
+            Console.WriteLine("p: " + p + " q: " + q);
+
+            m_modulus = p * q;
+            long phi = (p - 1) * (q - 1);
+
+            FindKeys(phi, p, q);
+        }
+        private static void FindKeys(long phi, long p, long q)
+        {
+            int i, d;
+            bool flag;
+            i = GetRandomPrime(2, 200);
+            while (i > phi)
+            {
+                i = GetRandomPrime(2, 200);
+            }
+            for (; i < phi; i++)
+            {
+                if (phi % i == 0)
+                    continue;
+                flag = IsPrime(i);
+                if (flag && i != p && i != q)
+                {
+                    m_publicKey = i;
+                    d = FindPrivateKey(phi);
+                    if (d > 0)
+                    {
+                        m_privateKey = d;
+                        break;
+                    }
+                }
+            }
+        }
+        private static int FindPrivateKey(long phi)
+        {
+            long i = 1;
+            while (true)
+            {
+                i += phi;
+                if (i % m_publicKey == 0)
+                {
+                    return ((int)i / m_publicKey);
+                }
+            }
+        }
+
+        public static long getPublicKey()
         {
             return m_publicKey;
         }
 
-        public static BigInteger getModulus()
+        public static long getModulus()
         {
             return m_modulus;
         }
 
-        public static byte[] encrypt(byte[] plaintext, int key, int modulus)
+        public static byte[] Encrypt(byte[] plaintext, int key, int modulus)
         {
-            int blockSize = getBlockSize();
-            List<byte[]> blocks = splitIntoBlocks(plaintext, blockSize);
-            List<byte[]> encryptedBlocks = new List<byte[]>();
+            List<byte> ecrypted = new List<byte>();
 
-            foreach (byte[] block in blocks)
+            foreach (byte ch in plaintext)
             {
-                //byte[] encryptedBlock = new byte[block.Length];
-                //List<byte[]> encryptedBlock = new List<byte[]>();
-                //int i = 0;
-                foreach(byte blockByte in block)
-                {
-
-                    BigInteger plaintextValue = new BigInteger(blockByte);
-                    BigInteger encryptedValue = BigInteger.ModPow(plaintextValue, key, modulus);
-                    //encryptedBlock[i] = encryptedValue.ToByteArray();
-                    encryptedBlocks.Add(encryptedValue.ToByteArray());
-                    //i++;
-                }
-
+                byte plainText = (byte)(ModPow((ch - 96), m_publicKey, m_modulus));
+                ecrypted.Add(plainText);
             }
 
-            return mergeBlocks(encryptedBlocks);
+            return ecrypted.ToArray();
         }
 
-        public static byte[] decrypt(byte[] ciphertext)
+        public static byte[] Decrypt(byte[] ciphertext)
         {
-            int blockSize = getBlockSize();
-            List<byte[]> blocks = splitIntoBlocks(ciphertext, blockSize);
-            List<byte[]> decryptedBlocks = new List<byte[]>();
+            List<byte> decrypted = new List<byte>();
 
-            foreach (byte[] block in blocks)
+            foreach (byte ch in ciphertext)
             {
-                BigInteger encryptedValue = new BigInteger(block);
-                BigInteger decryptedValue = BigInteger.ModPow(encryptedValue, m_privateKey, m_modulus);
-                byte[] decryptedBlock = decryptedValue.ToByteArray();
-
-                decryptedBlocks.Add(decryptedBlock);
+                byte plainText = (byte)(ModPow(ch + 96, m_privateKey, m_modulus));
+                decrypted.Add(plainText);
             }
 
-            return mergeBlocks(decryptedBlocks);
+            return decrypted.ToArray();
         }
 
-        private static List<byte[]> splitIntoBlocks(byte[] data, int blockSize)
+        private static bool IsPrime(int number)
         {
-            List<byte[]> blocks = new List<byte[]>();
-
-            int index = 0;
-            while (index < data.Length)
-            {
-                int remainingBytes = data.Length - index;
-                int blockSizeBytes = Math.Min(blockSize, remainingBytes);
-                byte[] block = new byte[blockSizeBytes];
-                Array.Copy(data, index, block, 0, blockSizeBytes);
-                blocks.Add(block);
-                index += blockSizeBytes;
-            }
-
-            return blocks;
-        }
-
-        private static byte[] mergeBlocks(List<byte[]> blocks)
-        {
-            int totalLength = 0;
-            foreach (byte[] block in blocks)
-            {
-                totalLength += block.Length;
-            }
-
-            byte[] merged = new byte[totalLength];
-            int index = 0;
-
-            foreach (byte[] block in blocks)
-            {
-                Array.Copy(block, 0, merged, index, block.Length);
-                index += block.Length;
-            }
-
-            return merged;
-        }
-
-        private static int getBlockSize()
-        {
-            return 5;
-        }
-
-        private static bool isPrime(int number)
-        {
-            if (number <= 1)
-            {
-                return false;
-            }
-
-            for (int i = 2; i <= Math.Sqrt(number); i++)
+            int i;
+            double j = Math.Sqrt(number);
+            for (i = 2; i <= j; i++)
             {
                 if (number % i == 0)
-                {
                     return false;
-                }
             }
-
             return true;
         }
-
-        private static int getRandomPrime(int min, int max)
+        private static long ModPow(int baseValue, int exponent, int modulus)
         {
-            int num = random.Next(min, max);
-            while(!isPrime(num))
+            long result = baseValue;
+            long j = 1;
+            for (int i = 0; i < exponent; i++)
             {
-                num = random.Next(min, max);
+                j *= result;
+                j %= modulus;
             }
-            return num;
-        }
-
-        private static BigInteger GCD(BigInteger a, BigInteger b)
-        {
-            if (b == 0)
-            {
-                return a;
-            }
-            return GCD(b, a % b);
-        }
-
-        private static int ModInverse(BigInteger a, int m)
-        {
-            a = a % m;
-
-            for (int x = 1; x < m; ++x)
-            {
-                if ((a * x) % m == 1)
-                {
-                    return x;
-                }
-            }
-
-            return -1; // Modular inverse does not exist
-        }
-
-        private int ModPow(int baseNum, int exponent, int modulus)
-        {
-            int result = 1;
-            baseNum = baseNum % modulus;
-
-            while (exponent > 0)
-            {
-                if (exponent % 2 == 1)
-                {
-                    result = (result * baseNum) % modulus;
-                }
-
-                exponent = exponent >> 1;
-                baseNum = (baseNum * baseNum) % modulus;
-            }
-
             return result;
         }
 
